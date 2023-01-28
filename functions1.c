f3decda2503af44f3981935300ee6d0d8c5023e0
Joshua Eze <96202637+Ebuka500@users.noreply.github.com>
2023-01-28 05:29:42 +0100

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -2,6 +2,7 @@
 
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
+ * collaboration
  * print_unsigned - Prints an unsigned number
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
